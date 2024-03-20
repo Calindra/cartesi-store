edf149dbfa47564bbfd6657af2b837f4422ca81d
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2024-03-20 10:19:06 -0300

    feat: erc-721 mint the contract from the voucher

---
+++ onchain/contracts/NonFunToken.sol
@@ -0,0 +1,46 @@
+// contracts/NonFunToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NonFunToken is ERC721, Ownable {
+
+    // Constructor will be called on contract creation
+    constructor() ERC721("NonFunToken", "NONFUN") {}
+
+    address private authority;
+    mapping(uint256 => string) private _tokenURIs;
+
+    /**
+     * @dev Sets the authority of the contract to mint new NFTs
+     */
+    function setAuthority(address _authority) public onlyOwner {
+        authority = _authority;
+    }
+
+    function getAuthority() public view returns (address) {
+        return authority;
+    }
+
+    function mintNFT(address collector, uint256 tokenId, string memory _tokenURI) public {
+        require(authority == msg.sender || owner() == msg.sender, "Only the authority can mint NFTs");
+        _safeMint(collector, tokenId); 
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+        return string(
+            abi.encodePacked(
+                _tokenURIs[tokenId],
+                tokenId,
+                '.json'
+            )
+        );
+    }
+}
---
+++ onchain/scripts/deploy.js
@@ -0,0 +1,36 @@
+// scripts/GLDToken_deploy.js
+
+const hre = require("hardhat");
+
+async function main() {
+  const NonFunToken = await hre.ethers.getContractFactory("NonFunToken");
+  console.log('Deploying NonFunToken...');
+  const token = await NonFunToken.deploy();
+
+  await token.deployed();
+  console.log("NonFunToken deployed to:", token.address);
+  const nonFunToken = await ethers.getContractAt('NonFunToken', token.address);
+
+  const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+  let tx = await nonFunToken.mintCollectionNFT(address, '0');
+  await tx.wait();
+  let tx1 = await nonFunToken.mintCollectionNFT(address, '1');
+  await tx1.wait();
+  let tx2 = await nonFunToken.mintCollectionNFT(address, '2');
+  await tx2.wait();
+  let tx3 = await nonFunToken.mintCollectionNFT(address, '3');
+  await tx3.wait();
+  let tx4 = await nonFunToken.mintCollectionNFT(address, '4');
+  await tx4.wait();
+  let tx5 = await nonFunToken.mintCollectionNFT(address, '5');
+  await tx5.wait();
+  console.log(`Mint NFT to ${address}`)
+}
+
+// module.exports = main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
