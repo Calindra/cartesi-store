cc2617342d87e6552a7365e0fd33f75fb15d2a2c
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2024-05-04 20:23:46 -0300

    build: update Cartesify libs

--- frontend/src/services/ConfigService.ts
+++ frontend/src/services/ConfigService.ts
@@ -1,5 +1,5 @@
 // you could check this address by executing `sunodo address-book`
-const DAPP_ADDRESS = '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C'
+const DAPP_ADDRESS = '0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e'
 
 export class ConfigService {
     static getDappAddress() {
